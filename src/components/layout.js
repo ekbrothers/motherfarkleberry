64035dcbfe15f28a0041e6780b23cb0b1f4ee4e1
Evan Brothers <evan.brothers@insight.com>
2023-06-02 10:29:26 -0400

    Update footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,10 +15,9 @@ export default function Layout(props) {
       <main className={styles.main}>{props.children}</main>
       <footer className={styles.footer}>
         <a href="https://gatsbyjs.com">Built with Gatsby</a>
-        <a href="https://github.com/gatsbyjs/gatsby-starter-landing-page">
+        <a href="https://github.com/ekbrothers/motherfarkleberry">
           View Source on GitHub
         </a>
-        <Link to="/demo">Demo</Link>
       </footer>
     </div>
   );
